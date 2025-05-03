12de5eaa977ad92560a43109bbe4270dba16edbe
Leonardo Capristo <leonardo.capristo@hotmail.com>
2025-05-03 08:45:44 -0300

    delete

--- src/main/java/io/github/leonardocapristo/cadastroclientesapi/controllers/UserController.java
+++ src/main/java/io/github/leonardocapristo/cadastroclientesapi/controllers/UserController.java
@@ -3,9 +3,14 @@ package io.github.leonardocapristo.cadastroclientesapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +31,28 @@ public class UserController {
         return services.buscarTodos();
     }
     
+    @GetMapping(value = "/filtros")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<UserDTO> buscarTodosPaginaFiltros(Pageable pageable) {
+        return services.buscarTodosPaginaFiltros(pageable);
+    }
+    
     @GetMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public UserDTO buscarPorId(@PathVariable Long id) {
         return services.buscarPorId(id);
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO adicionarNovo(@RequestBody UserDTO userDTO) {
+        return services.adicionarNovo(userDTO);
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id) {
+        services.deletar(id);
     }
 
 }
--- src/main/java/io/github/leonardocapristo/cadastroclientesapi/services/UserServices.java
+++ src/main/java/io/github/leonardocapristo/cadastroclientesapi/services/UserServices.java
@@ -4,39 +4,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.stereotype.Service;
 
 import io.github.leonardocapristo.cadastroclientesapi.dto.UserDTO;
 import io.github.leonardocapristo.cadastroclientesapi.entities.User;
+import io.github.leonardocapristo.cadastroclientesapi.exceptions.FiltrosInvalidosException;
 import io.github.leonardocapristo.cadastroclientesapi.exceptions.IdNaoEncontradoException;
 import io.github.leonardocapristo.cadastroclientesapi.repositories.UserRepository;
 
 @Service
 public class UserServices {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<UserDTO> buscarTodos() {
-		
+
 		List<User> lista = repository.findAll();
-		
+
 		List<UserDTO> listaDTO = new ArrayList<>();
-		
+
 		for (User user : lista) {
 			listaDTO.add(new UserDTO(user));
 		}
-		
+
 		return listaDTO;
-	
+
 	}
-	
-	
+
+	public Page<UserDTO> buscarTodosPaginaFiltros(Pageable pageable) {
+
+		try {
+			// parametros page,size e sort
+
+			Page<User> lista = repository.findAll(pageable);
+			return lista.map(user -> new UserDTO(user));
+
+		} catch (PropertyReferenceException e) {
+			throw new FiltrosInvalidosException("Filtros de busca inválidos");
+		}
+
+	}
+
 	public UserDTO buscarPorId(Long id) {
-		
-		
-	    User entity = repository.findById(id).orElseThrow(() -> new IdNaoEncontradoException("ID do cliente não encontrado"));
+
+		User entity = repository.findById(id)
+				.orElseThrow(() -> new IdNaoEncontradoException("ID do usuario não encontrado"));
 		return new UserDTO(entity);
+
+	}
+
+	public UserDTO adicionarNovo(UserDTO userDTO) {
+
+		User entity = new User();
+
+		entity.setFirstName(userDTO.getEmail());
+		entity.setLastName(userDTO.getLastName());
+		entity.setEmail(userDTO.getEmail());
+		entity.setPassword(userDTO.getPassword());
+
+		repository.save(entity);
+
+		return new UserDTO(entity);
+
+	}
+
+	public void deletar(Long id) {
+
+		User entity = repository.findById(id)
+				.orElseThrow(() -> new IdNaoEncontradoException("ID do usuario não encontrado"));
+
+		repository.deleteById(entity.getId());
 
 	}
 }
